98cfe61c22648a39af9d03d856c5d16fa4399c6c
werton <mr.werton@gmail.com>
2024-02-15 15:26:24 -0300

    feat: created a alg to show a  msg

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function showMessage(mgs){
+    console.log(mgs )
+}
+
+
+const description = `Hello, i'm Werton Silva!! i'm software developer student and already has experience of three years with sofrware development. My experiences approach mobile, web, backend and ml area.`
+
+showMessage(description)
